b9544c8aca010d1d683c5f25b4d4c877ce56f99f
griffin-stewie <panterathefamilyguy@gmail.com>
2021-12-10 23:36:21 +0900

    Initial Commit

---
+++ macOSBeacon/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  macOSBeacon
+//
+//  Created by griffin-stewie on 2021/12/10.
+//  
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ macOSBeacon/macOSBeaconApp.swift
@@ -0,0 +1,18 @@
+//
+//  macOSBeaconApp.swift
+//  macOSBeacon
+//
+//  Created by griffin-stewie on 2021/12/10.
+//  
+//
+
+import SwiftUI
+
+@main
+struct macOSBeaconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
